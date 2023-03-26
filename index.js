d83c784f4aa4224ee345f049afec28dceea1c1d7
Rock N Rome <126816805+rocknrome@users.noreply.github.com>
2023-03-26 11:42:55 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+//Create two numbers and compare them. Then display the largest number.
+  let numberOne = 5;
+  let numberTwo = 7;
+console.log('Between the two given numbers ' + (numberOne) + ' and ' + (numberTwo)) 
+if (numberOne > numberTwo) {
+  console.log((numberOne) + ' is a larger number');
+}
+else {
+  console.log((numberTwo) + ' is a larger number');
+}
