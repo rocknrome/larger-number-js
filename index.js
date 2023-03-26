84a60f937b7249ecde43bdfff30784a7487cbd40
Rock N Rome <126816805+rocknrome@users.noreply.github.com>
2023-03-26 12:37:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 //Create two numbers and compare them. Then display the largest number.
-  let numberOne = 5;
-  let numberTwo = 7;
+  let numberOne = 3;
+  let numberTwo = 3;
 console.log('Between the two given numbers ' + (numberOne) + ' and ' + (numberTwo)) 
 if (numberOne > numberTwo) {
   console.log((numberOne) + ' is a larger number');
 }
-else {
+else if (numberOne < numberTwo) {
   console.log((numberTwo) + ' is a larger number');
+}
+if (numberOne === numberTwo) {
+  console.log('both numbers are equal');
 }
